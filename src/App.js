ce87c83a950115855a51a2cf24406644db4f6b6d
Veta Sheina <sssheina@gmail.com>
2023-01-26 19:19:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <h1>Hello World!</h1>
     </div>
   );
 }
